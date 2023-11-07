cc64697e83e16012f83643128f78a851aa7f19fb
Michael Schwab <michael.sc.schwab@partner.bmwgroup.com>
2023-11-07 03:11:36 +0100

    Adds more weather data

--- app/src/main/java/com/example/netlight_weather/ui/data/WeatherLocationCardViewModel.kt
+++ app/src/main/java/com/example/netlight_weather/ui/data/WeatherLocationCardViewModel.kt
@@ -19,25 +19,55 @@ class WeatherLocationCardViewModel: ViewModel() {
         }
 
     private fun getWeatherDataForLocation(cityName: String): WeatherLocationCardInput? {
-        if (cityName != "Munich") {
-            return null
+        when (cityName) {
+            "Munich" -> {
+                return WeatherLocationCardInput(
+                    icon = R.drawable.ic_heavysnow,
+                    currentTemperature = 20.0,
+                    maxTemperature = 23.0,
+                    minTemperature = 10.0,
+                    wind = 3.5,
+                    rain = 1.8,
+                    weatherDayOverviewInputs = listOf<WeatherDayOverviewInput>(
+                        WeatherDayOverviewInput("Monday", 10.0, 12.0, R.drawable.ic_cloudy),
+                        WeatherDayOverviewInput("Tuesday", 11.0, 13.0, R.drawable.ic_snowy),
+                        WeatherDayOverviewInput("Wednesday", 12.0, 14.0, R.drawable.ic_rainshower),
+                        WeatherDayOverviewInput("Thursday", 13.0, 15.0, R.drawable.ic_thunder),
+                        WeatherDayOverviewInput("Friday", 14.0, 16.0, R.drawable.ic_heavysnow),
+                        WeatherDayOverviewInput("Saturday", 15.0, 17.0, R.drawable.ic_verycloudy),
+                        WeatherDayOverviewInput("Sunday", 16.0, 18.0, R.drawable.ic_rainshower),
+                    )
+                )
+            }
+            "Stockholm" -> {
+                return WeatherLocationCardInput(
+                    icon = R.drawable.ic_heavysnow,
+                    currentTemperature = 20.0,
+                    maxTemperature = 23.0,
+                    minTemperature = 10.0,
+                    wind = 3.5,
+                    rain = 1.8,
+                    weatherDayOverviewInputs = listOf<WeatherDayOverviewInput>(
+                        WeatherDayOverviewInput("Monday", 10.0, 12.0, R.drawable.ic_sunny),
+                        WeatherDayOverviewInput("Tuesday", 11.0, 13.0, R.drawable.ic_sunny),
+                        WeatherDayOverviewInput("Wednesday", 12.0, 14.0, R.drawable.ic_sunny),
+                        WeatherDayOverviewInput("Thursday", 13.0, 15.0, R.drawable.ic_sunny),
+                        WeatherDayOverviewInput("Friday", 14.0, 16.0, R.drawable.ic_sunny),
+                        WeatherDayOverviewInput("Saturday", 15.0, 17.0, R.drawable.ic_verycloudy),
+                        WeatherDayOverviewInput("Sunday", 16.0, 18.0, R.drawable.ic_rainshower),
+                    )
+                )
+            }
+            else -> {
+                return null
+            }
         }
-        return WeatherLocationCardInput(
-            icon = R.drawable.ic_heavysnow,
-            currentTemperature = 20.0,
-            maxTemperature = 23.0,
-            minTemperature = 10.0,
-            wind = 3.5,
-            rain = 1.8,
-            weatherDayOverviewInputs = listOf<WeatherDayOverviewInput>(
-                WeatherDayOverviewInput("Monday", 10.0, 12.0, R.drawable.ic_cloudy),
-                WeatherDayOverviewInput("Tuesday", 11.0, 13.0, R.drawable.ic_snowy),
-                WeatherDayOverviewInput("Wednesday", 12.0, 14.0, R.drawable.ic_rainshower),
-                WeatherDayOverviewInput("Thursday", 13.0, 15.0, R.drawable.ic_thunder),
-                WeatherDayOverviewInput("Friday", 14.0, 16.0, R.drawable.ic_heavysnow),
-                WeatherDayOverviewInput("Saturday", 15.0, 17.0, R.drawable.ic_verycloudy),
-                WeatherDayOverviewInput("Sunday", 16.0, 18.0, R.drawable.ic_rainshower),
-            )
-        )
+
     }
 }
+
+data class WeatherLocationState(
+    val error:String?,
+    val weatherLocationCardInput: WeatherLocationCardInput,
+    val isLoading: Boolean
+)
