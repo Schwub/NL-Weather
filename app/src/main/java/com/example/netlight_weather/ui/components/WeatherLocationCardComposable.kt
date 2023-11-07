1a9cc76b08fbe8ccb4dd2863d7f0094b1ce74acd
Michael Schwab <michael.sc.schwab@partner.bmwgroup.com>
2023-11-07 02:50:38 +0100

    Adds ViewModel

--- app/src/main/java/com/example/netlight_weather/ui/components/WeatherLocationCardComposable.kt
+++ app/src/main/java/com/example/netlight_weather/ui/components/WeatherLocationCardComposable.kt
@@ -12,20 +12,35 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.netlight_weather.R
+import com.example.netlight_weather.ui.data.WeatherLocationCardViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherLocationCardComposable(
-    weatherLocationCardInput: WeatherLocationCardInput
+    initialCityName: String,
 ) {
+    val weatherLocationCardViewModel: WeatherLocationCardViewModel = viewModel()
+    var cityName by remember { mutableStateOf(initialCityName) }
+    weatherLocationCardViewModel.cityName = cityName
+    val weatherLocationCardInput = weatherLocationCardViewModel.weatherData
+
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -33,67 +48,77 @@ fun WeatherLocationCardComposable(
             .wrapContentHeight()
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
-            Text(weatherLocationCardInput.cityName, style = MaterialTheme.typography.headlineLarge)
-            Spacer(modifier = Modifier.padding(4.dp))
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-            ) {
-                Text(text = "${weatherLocationCardInput.currentTemperature} °", style = MaterialTheme.typography.headlineMedium)
-                Image(
-                    painter = painterResource(id = weatherLocationCardInput.icon),
-                    contentDescription = "Test",
-                )
-            }
-            Row {
-                Column {
-                    Row(Modifier.height(20.dp)) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.keyboard_double_arrow_up),
-                            contentDescription = "Test"
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(text = "${weatherLocationCardInput.maxTemperature} °")
+            OutlinedTextField(
+                value = cityName,
+                label = {Text(text = "City")},
+                onValueChange = { cityName = it },
+                textStyle = MaterialTheme.typography.headlineMedium
+            )
+            if (weatherLocationCardInput == null) {
+                Text(text = "No Data", style = MaterialTheme.typography.labelLarge)
+            } else {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = "${weatherLocationCardInput.currentTemperature} °",
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                    Image(
+                        painter = painterResource(id = weatherLocationCardInput.icon),
+                        contentDescription = "Test",
+                    )
+                }
+                Row {
+                    Column {
+                        Row(Modifier.height(20.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.keyboard_double_arrow_up),
+                                contentDescription = "Test"
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = "${weatherLocationCardInput.maxTemperature} °")
+                        }
+                        Spacer(modifier = Modifier.height(2.dp))
+                        Row(Modifier.height(20.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.keyboard_double_arrow_down),
+                                contentDescription = "Test"
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = "${weatherLocationCardInput.minTemperature} °")
+                        }
                     }
-                    Spacer(modifier = Modifier.height(2.dp))
-                    Row(Modifier.height(20.dp)) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.keyboard_double_arrow_down),
-                            contentDescription = "Test"
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(text = "${weatherLocationCardInput.minTemperature} °")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Column {
+                        Row(Modifier.height(20.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_rain),
+                                contentDescription = "Test"
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = "${weatherLocationCardInput.rain} mm")
+                        }
+                        Spacer(modifier = Modifier.height(2.dp))
+                        Row(Modifier.height(20.dp)) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_wind),
+                                contentDescription = "Test"
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(text = "${weatherLocationCardInput.wind} m/s")
+                        }
                     }
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-                Column {
-                    Row(Modifier.height(20.dp)) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_rain),
-                            contentDescription = "Test"
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(text = "${weatherLocationCardInput.rain} mm")
-                    }
-                    Spacer(modifier = Modifier.height(2.dp))
-                    Row(Modifier.height(20.dp)) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.ic_wind),
-                            contentDescription = "Test"
-                        )
-                        Spacer(modifier = Modifier.width(4.dp))
-                        Text(text = "${weatherLocationCardInput.wind} m/s")
-                    }
-                }
+                WeatherWeekOverviewComposable(weatherDayOverviewInputs = weatherLocationCardInput.weatherDayOverviewInputs)
             }
         }
-        WeatherWeekOverviewComposable(weatherDayOverviewInputs = weatherLocationCardInput.weatherDayOverviewInputs)
     }
 }
 
 data class WeatherLocationCardInput(
-    val cityName: String,
     @DrawableRes
     val icon: Int,
     val currentTemperature: Double,
@@ -107,24 +132,5 @@ data class WeatherLocationCardInput(
 @Preview
 @Composable
 fun PreviewWeatherLocationCardComposable() {
-    WeatherLocationCardComposable(previewData)
+    WeatherLocationCardComposable(initialCityName = "Munich")
 }
-
-val previewData = WeatherLocationCardInput(
-    cityName = "Munich",
-    icon = R.drawable.ic_heavysnow,
-    currentTemperature = 20.0,
-    maxTemperature = 23.0,
-    minTemperature = 10.0,
-    wind = 3.5,
-    rain = 1.8,
-    weatherDayOverviewInputs = listOf<WeatherDayOverviewInput>(
-        WeatherDayOverviewInput("Monday", 10.0, 12.0, R.drawable.ic_cloudy),
-        WeatherDayOverviewInput("Tuesday", 11.0, 13.0, R.drawable.ic_snowy),
-        WeatherDayOverviewInput("Wednesday", 12.0, 14.0, R.drawable.ic_rainshower),
-        WeatherDayOverviewInput("Thursday", 13.0, 15.0, R.drawable.ic_thunder),
-        WeatherDayOverviewInput("Friday", 14.0, 16.0, R.drawable.ic_heavysnow),
-        WeatherDayOverviewInput("Saturday", 15.0, 17.0, R.drawable.ic_verycloudy),
-        WeatherDayOverviewInput("Sunday", 16.0, 18.0, R.drawable.ic_rainshower),
-    )
-)
---
+++ app/src/main/java/com/example/netlight_weather/ui/data/WeatherLocationCardViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.netlight_weather.ui.data
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.netlight_weather.R
+import com.example.netlight_weather.ui.components.WeatherDayOverviewInput
+import com.example.netlight_weather.ui.components.WeatherLocationCardInput
+
+class WeatherLocationCardViewModel: ViewModel() {
+
+    var weatherData: WeatherLocationCardInput? by mutableStateOf(null)
+    var cityName: String? = null
+        set(value) {
+            field = value
+            weatherData = value?.let { getWeatherDataForLocation(it) }
+        }
+
+    private fun getWeatherDataForLocation(cityName: String): WeatherLocationCardInput? {
+        if (cityName != "Munich") {
+            return null
+        }
+        return WeatherLocationCardInput(
+            icon = R.drawable.ic_heavysnow,
+            currentTemperature = 20.0,
+            maxTemperature = 23.0,
+            minTemperature = 10.0,
+            wind = 3.5,
+            rain = 1.8,
+            weatherDayOverviewInputs = listOf<WeatherDayOverviewInput>(
+                WeatherDayOverviewInput("Monday", 10.0, 12.0, R.drawable.ic_cloudy),
+                WeatherDayOverviewInput("Tuesday", 11.0, 13.0, R.drawable.ic_snowy),
+                WeatherDayOverviewInput("Wednesday", 12.0, 14.0, R.drawable.ic_rainshower),
+                WeatherDayOverviewInput("Thursday", 13.0, 15.0, R.drawable.ic_thunder),
+                WeatherDayOverviewInput("Friday", 14.0, 16.0, R.drawable.ic_heavysnow),
+                WeatherDayOverviewInput("Saturday", 15.0, 17.0, R.drawable.ic_verycloudy),
+                WeatherDayOverviewInput("Sunday", 16.0, 18.0, R.drawable.ic_rainshower),
+            )
+        )
+    }
+}
